e60cdc958a9d25edfc6cc370c3570dbdc9471b51
adamthompson0 <mrthompson11@outlook.com>
2023-08-14 13:51:02 -0700

    Finish Todo app

--- TodoList/todos/admin.py
+++ TodoList/todos/admin.py
@@ -1,3 +1,3 @@
 from django.contrib import admin
-
+from .models import Todo, Priority
 # Register your models here.
--- TodoList/todos/models.py
+++ TodoList/todos/models.py
@@ -1,3 +1,20 @@
 from django.db import models
-
+from django.contrib.auth.models import User
 # Create your models here.
+
+class Priority(models.Model):
+    text = models.CharField(max_length=50)
+    value = models.IntegerField()
+
+    def __str__(self):
+        return self.text
+    
+class Todo(models.Model):
+    text = models.CharField(max_length=100)
+    created_at = models.DateTimeField(auto_now_add=True)
+    is_completed = models.BooleanField(default=False)
+    priority = models.ForeignKey(Priority,on_delete=models.CASCADE)
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.text
